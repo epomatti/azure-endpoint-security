b558abbdcd7100a3bce21f1acad97c559fe04ab6
Evandro Pomatti <evandro.pomatti@gmail.com>
2024-12-31 01:32:52 -0300

    fix: dynamic output values

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "deallocate_windows_desktop_ssh_command" {
-  value = "az vm deallocate --resource-group ${azurerm_resource_group.default.name} --name ${module.vm_windows_desktop[0].name}"
+  value = var.create_windows_desktop ? "az vm deallocate --resource-group ${azurerm_resource_group.default.name} --name ${module.vm_windows_desktop[0].name}" : ""
 }
 
 output "start_windows_desktop_ssh_command" {
-  value = "az vm start --resource-group ${azurerm_resource_group.default.name} --name ${module.vm_windows_desktop[0].name}"
+  value = var.create_windows_desktop ? "az vm start --resource-group ${azurerm_resource_group.default.name} --name ${module.vm_windows_desktop[0].name}" : ""
 }

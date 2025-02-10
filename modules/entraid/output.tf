75f238d30e3e0a24bba98f91dd836956793a686e
Evandro Pomatti <evandro.pomatti@gmail.com>
2025-02-10 20:56:40 -0300

    feat: endpoint output for user upn

--- modules/entraid/output.tf
+++ modules/entraid/output.tf
@@ -5,3 +5,11 @@ output "administrator_user_object_id" {
 output "endpoint_user_object_id" {
   value = azuread_user.endpoint_user.object_id
 }
+
+output "administrator_user_upn" {
+  value = azuread_user.administrator.user_principal_name
+}
+
+output "endpoint_user_upn" {
+  value = azuread_user.endpoint_user.user_principal_name
+}
--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "deallocate_windows_desktop_ssh_command" {
 output "start_windows_desktop_ssh_command" {
   value = var.create_windows_desktop ? "az vm start --resource-group ${azurerm_resource_group.default.name} --name ${module.vm_windows_desktop[0].name}" : ""
 }
+
+output "entraid_administrator_upn" {
+  value = module.entraid.administrator_user_upn
+}
+
+output "entraid_endpoint_upn" {
+  value = module.entraid.endpoint_user_upn
+}

44c3ca4e2bc29a0fdfa2728a14f680ad95a70495
Evandro Pomatti <evandro.pomatti@gmail.com>
2025-06-07 20:30:39 -0300

    nit: removed deprecated attribute

--- modules/vm/windows-desktop/main.tf
+++ modules/vm/windows-desktop/main.tf
@@ -36,9 +36,8 @@ resource "azurerm_windows_virtual_machine" "default" {
   admin_password        = var.windows_desktop_admin_password
   network_interface_ids = [azurerm_network_interface.default.id]
 
-  secure_boot_enabled               = true
-  vtpm_enabled                      = true
-  vm_agent_platform_updates_enabled = true
+  secure_boot_enabled = true
+  vtpm_enabled        = true
 
   os_disk {
     name                 = "osdisk-${local.name}"
